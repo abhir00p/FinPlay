4d29f9235a93da76e548d1727452a3a627409d8b
abhir00p <inbox4abhirup@gmail.com>
2024-08-12 23:16:01 +0530

    fileStruct_flexBetween_navbar_themes_palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-function App() {
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline, dividerClasses, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []) //"[]" tells react to only create the theme once and not on every render
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem"> 
+            <Navbar/>
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+                />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App
