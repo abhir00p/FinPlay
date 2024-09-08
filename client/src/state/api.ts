1eeb971cdc95ca5293be7ae9aa1b6d20b75b08e4
abhir00p <inbox4abhirup@gmail.com>
2024-09-08 20:35:27 +0530

    Fixed_api_fetch

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
     tagTypes: [],
     endpoints: (build) => ({
         getKpis: build.query<void, void>({
-            query: () => "kpi/kpis/",
+            query: () => "kpi/kpis",
             providesTags: ["Kpis"],
         }),
     })
--- server/routes/kpi.js
+++ server/routes/kpi.js
@@ -3,7 +3,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get("kpis", async(req, res) => {
+router.get("/kpis", async(req, res) => {
     try {
         const kpis = await KPI.find();
         res.status(200).json(kpis);
